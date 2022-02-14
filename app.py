f6a3917347008aa3746643978947ae58beedab69
Greg Morton <gregorymorton96@gmail.com>
2022-02-14 11:47:06 -0500

    return csv list

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ app = Flask(__name__)  # Note the double underscores on each side!
 def get_csv(): # define csv file path
     csv_path = './static/la-riots-deaths.csv'
     csv_file = open(csv_path, 'r')
-    csv_obj = csv.DictReader(csv_file)
+    csv_obj = csv.DictReader(csv_file) # take the header row and combine each header with each value
     csv_list = list(csv_obj)
+    return csv_list
 
 
 # This is the file that will serve as your application’s “backend,” routing data to the appropriate pages.
