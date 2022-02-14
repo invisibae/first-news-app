b01bd570afd91fe1119ce7347af210e3322b3507
Greg Morton <gregorymorton96@gmail.com>
2022-02-14 11:24:49 -0500

    def index

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask # Flask is a framework for designing websites # we'll write everything here in one file
-from flask import render_template # Render template function to render a page from a template 
+from flask import render_template # Render template function to render a page from a template
 app = Flask(__name__)  # Note the double underscores on each side!
 
 # This is the file that will serve as your application’s “backend,” routing data to the appropriate pages.
+def index(): # Then create a function called index that returns our rendered index.html template
+    template = 'index.html'
+    return render_template(template)
+
 
 if __name__ == '__main__':
     # Fire up the Flask test server
