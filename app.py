8d8db2e381556343355cd87bcb5bfdf93bfb74cb
Greg Morton <gregorymorton96@gmail.com>
2022-02-14 11:12:44 -0500

    please debug my problems for me

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
-from flask import Flask
+from flask import Flask # Flask is a framework for designing websites # we'll write everything here in one file
 app = Flask(__name__)  # Note the double underscores on each side!
+
+# This is the file that will serve as your application’s “backend,” routing data to the appropriate pages.
+
+if __name__ == '__main__':
+    # Fire up the Flask test server
+    app.run(debug=True, use_reloader=True)
