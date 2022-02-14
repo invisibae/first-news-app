a4623972556543055a0ee2b0c4a20a2bda0914c1
Greg Morton <gregorymorton96@gmail.com>
2022-02-14 11:43:30 -0500

    add csv file path

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+import csv # import csv
 from flask import Flask # Flask is a framework for designing websites # we'll write everything here in one file
 from flask import render_template # Render template function to render a page from a template
 app = Flask(__name__)  # Note the double underscores on each side!
+
+def get_csv(): # define csv file path
+    csv_path = './static/la-riots-deaths.csv'
+    csv_file = open(csv_path, 'r')
+
 
 # This is the file that will serve as your application’s “backend,” routing data to the appropriate pages.
 @app.route("/") # Now use one of Flask’s coolest tricks, the app.route decorator, to connect that function with the root URL of our site, /.
